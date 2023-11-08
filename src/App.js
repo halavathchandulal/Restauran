04e2e66d43d32cc592cb9f1973c254b0f9f15dea
chandulal <halavathchandu2000@gmail.com>
2023-11-08 10:53:08 +0000

    Restaurant App

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// App.js
+// Remove the 'React' import as it's not used
+import UniRestoCafe from './components/UniRestoCafe'
+import './App.css'
+
+const App = () => (
+  <div className="App">
+    <UniRestoCafe />
+  </div>
+)
+
+export default App
